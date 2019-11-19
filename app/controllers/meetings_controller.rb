d91c346312b6ebd611e2f33de2e858cc91beb705
u-sho-21 <u.s21495@gmail.com>
2019-11-19 14:28:41 +0900

    gitテスト

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,4 +1,80 @@
 class MeetingsController < ApplicationController
+  before_action :correct_teacher,   only: [:new]
+
+  # 面談日時登録ページ
   def new
+    @meeting = @teacher.meetings.build
+    @meetings = @teacher.meetings.all
+    @hour = (0..23).to_a.map { |v| "%02d" % v }
+    @minutes = (0..59).to_a.map { |v| "%02d" % v }
+    @frame_list = [1,2,3,4,5,6,7,8]
   end
+
+  # 面談日レコード作成
+  def create
+    if params[:commit] == "登録"
+      unless params[:meeting][:date].first.empty?
+        Meeting.where(user_id: @user.id).delete_all
+        MeetingTime.where(user_id: @user.id).delete_all
+        params[:meeting][:date].split("\r\n").each do |day|
+          unless @user.meetings.any? {|meeting| meeting.date == day}
+            @user.children.where(user_id: @user).each do |child|
+              record = @user.meetings.build(date: day, child_id: child.id)
+              record.save
+            end
+          end
+        end
+        flash[:success] = "面談日を登録しました。次に面談時間を登録してください。"
+        redirect_to new_user_meeting_url
+      else
+        flash[:danger] = "入力がありません。"
+        redirect_to new_user_meeting_url
+      end
+    end
+
+    if params[:commit] == "追加"
+      @started_time_1 = params[:published_at_hour]
+      @started_time_2 = params[:published_at_minute_1]
+      @minutes = params[:published_at_minute_2]
+      @frame = params[:frame]
+      addtime_1 = @started_time_1.to_i
+      addtime_2 = @started_time_2.to_i
+      @times = times(addtime_1, addtime_2, @frame, @minutes)
+
+      unless @user.meetings.any? {|meeting| meeting.date == params[:date]}
+        @user.children.where(user_id: @user).each do |child|
+          record = @user.meetings.build(date: params[:date], child_id: child.id)
+          record.save
+        end
+      end
+
+      for i in 0..@times.length-1 do
+        addtime = params[:date] + " #{@times[i]}"  # 右側の""内の半角スペースは必要なため消さない
+        recode = @user.meeting_times.build(time: Time.zone.parse(addtime))
+        recode.save
+      end
+      redirect_to user_meeting_time_index_url(@user)
+    end
+
+    if params[:commit] == "初期化"
+      Meeting.where(user_id: @user.id).delete_all
+      MeetingTime.where(user_id: @user.id).delete_all
+      flash[:danger] = "面談日程を初期化しました。"
+      redirect_to new_user_meeting_url
+    end
+  end
+
+  private
+
+    # before_action
+
+    # 現在ログインしている教員本人かどうか
+    def correct_teacher
+      @teacher = Teacher.find(params[:teacher_id])
+      unless current_teacher?(@teacher)
+        flash[:danger] = "ユーザー本人しかアクセスできません。"
+        redirect_to(root_url)
+      end
+    end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,27 +1,58 @@
 module SessionsHelper
-  
+
+  # 保護者ログインセッション
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
+  # 教員ログインセッション
+  def log_in_teacher(teacher)
+    session[:teacher_id] = teacher.id
+  end
+
+  # 保護者remember
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # 教員remember
+  def remember_teacher(teacher)
+    teacher.remember
+    cookies.permanent.signed[:user_id] = teacher.id
+    cookies.permanent[:remember_token] = teacher.remember_token
+  end
+
+  # 保護者forget
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
-  
+
+  # 教員forget
+  def forget_teacher(teacher)
+    teacher.forget
+    cookies.delete(:teacher_id)
+    cookies.delete(:teacher_token)
+  end
+
+  # 保護者ログアウト
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
+  # 教員ログアウト
+  def log_out_teacher
+    forget(current_teacher)
+    session.delete(:teacher_id)
+    @current_user = nil
+  end
+
+  # 保護者現在ユーザー
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -33,8 +64,27 @@ module SessionsHelper
       end
     end
   end
-  
+
+  # 教員現在ユーザー
+  def current_teacher
+    if (teacher_id = session[:teacher_id])
+      @current_teacher ||= Teacher.find_by(id: teacher_id)
+    elsif (teacher_id = cookies.signed[:teacher_id])
+      teacher = Teacher.find_by(id: teacher_id)
+      if teacher && teacher.authenticated?(cookies[:remember_token])
+        log_in teacher
+        @current_teacher = teacher
+      end
+    end
+  end
+
+  # 保護者ログイン
   def logged_in?
     !current_user.nil?
+  end
+
+  # 教員ログイン
+  def logged_in_teacher?
+    !current_teacher.nil?
   end
 end
