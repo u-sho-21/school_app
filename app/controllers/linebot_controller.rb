f23c1d592703e93aae6359e3a78fb26488ecb4f4
yukiabineko <Yuki14070912>
2019-12-13 18:52:54 +0900

    line通知微調整

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -100,7 +100,7 @@ class LinebotController < ApplicationController
 
     if params[:commit] == "日時確定送信"
       require 'line/bot'
-      debugger
+      
 
       client = Line::Bot::Client.new { |config|
         config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
