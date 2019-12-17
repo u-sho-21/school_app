5cc9edf3e2a8cdab7984ccb6a5a0bff45a27aff9
u-sho-21 <u.s21495@gmail.com>
2019-12-17 19:40:54 +0900

    連絡機能のバリデーション関連2

--- app/models/p_message.rb
+++ app/models/p_message.rb
@@ -2,7 +2,7 @@ class PMessage < ApplicationRecord
   belongs_to :user
   belongs_to :teacher
 
-  validates :title,  presence: true, length: { maximum: 15 }
+  validates :title,  presence: true, length: { maximum: 20 }
   validates :content,  presence: true, length: { maximum: 100 }
   validates :reply, length: { maximum: 100 }
 end
