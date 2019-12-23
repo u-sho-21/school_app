7bfc9de7f6f0b85ca3ddf93b9ecdc3f2d92d7231
u-sho-21 <u.s21495@gmail.com>
2019-12-23 18:04:16 +0900

    lineテスト4

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -13,84 +13,84 @@ class LinebotController < ApplicationController
         config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
       }
 
-      # message={
-      # type: 'text',
-      # text: 'テスト'
-      # }
+      message={
+      type: 'text',
+      text: 'テスト'
+      }
 
       # "url": "https://push-test-pta.herokuapp.com/ファイル名(拡張子も)"   publicの場合
       # "url": "https://drive.google.com/uc?id=ファイルID"                googledriveの場合
       # "url": "埋め込みurl"                                              onedriveの場合
       image_url = "https://school-app-pta.herokuapp.com/image01.jpg"
 
-      message={
-        "type": "flex",
-        "altText": "this is a flex message",
-        "contents": {
-          "type": "bubble",
-          "hero": {
-            "type": "image",
-            "url": image_url,
-            "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover"
-          },
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "md",
-            "contents": [
-              {
-                "type": "text",
-                "text": "面談のお知らせ",
-                "size": "xl",
-                "weight": "bold"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "下記からサイトにアクセスして、\n希望日等の登録をお願いします。",
-                        "weight": "bold",
-                        "margin": "sm",
-                        "wrap": true,
-                        "flex": 0
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          },
-          "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "spacer",
-                "size": "xxl"
-              },
-              {
-                "type": "button",
-                "style": "primary",
-                "color": "#FF9933",
-                "action": {
-                  "type": "uri",
-                  "label": "サイトへ",
-                  "uri": "https://school-app-pta.herokuapp.com"
-                }
-              }
-            ]
-          }
-        }
-      }
+      # message={
+      #   "type": "flex",
+      #   "altText": "this is a flex message",
+      #   "contents": {
+      #     "type": "bubble",
+      #     "hero": {
+      #       "type": "image",
+      #       "url": image_url,
+      #       "size": "full",
+      #       "aspectRatio": "20:13",
+      #       "aspectMode": "cover"
+      #     },
+      #     "body": {
+      #       "type": "box",
+      #       "layout": "vertical",
+      #       "spacing": "md",
+      #       "contents": [
+      #         {
+      #           "type": "text",
+      #           "text": "面談のお知らせ",
+      #           "size": "xl",
+      #           "weight": "bold"
+      #         },
+      #         {
+      #           "type": "box",
+      #           "layout": "vertical",
+      #           "spacing": "sm",
+      #           "contents": [
+      #             {
+      #               "type": "box",
+      #               "layout": "baseline",
+      #               "contents": [
+      #                 {
+      #                   "type": "text",
+      #                   "text": "下記からサイトにアクセスして、\n希望日等の登録をお願いします。",
+      #                   "weight": "bold",
+      #                   "margin": "sm",
+      #                   "wrap": true,
+      #                   "flex": 0
+      #                 }
+      #               ]
+      #             }
+      #           ]
+      #         }
+      #       ]
+      #     },
+      #     "footer": {
+      #       "type": "box",
+      #       "layout": "vertical",
+      #       "contents": [
+      #         {
+      #           "type": "spacer",
+      #           "size": "xxl"
+      #         },
+      #         {
+      #           "type": "button",
+      #           "style": "primary",
+      #           "color": "#FF9933",
+      #           "action": {
+      #             "type": "uri",
+      #             "label": "サイトへ",
+      #             "uri": "https://school-app-pta.herokuapp.com"
+      #           }
+      #         }
+      #       ]
+      #     }
+      #  }
+      # }
 
       group_id = ENV["LINE_CHANNEL_GROUP_ID"]
       response = client.push_message(group_id, message)
