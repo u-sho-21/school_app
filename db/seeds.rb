51db0780115dcbc8c75c2d35263f2f0272d0b885
u-sho-21 <u.s21495@gmail.com>
2020-01-19 15:57:39 +0900

    test seedファイルコメントアウト

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,34 +3,34 @@
 require 'faker/japanese'
 
 #初期書類作成用ユーザー
-User.create!(name: "学校",
-             name2:"管理者",
-             email: "admon@example.com",
-             phone:"111111",
-             password: "password",
-             password_confirmation: "password")
+# User.create!(name: "学校",
+#              name2:"管理者",
+#              email: "admon@example.com",
+#              phone:"111111",
+#              password: "password",
+#              password_confirmation: "password")
 
 
-Teacher.create!( name: "教員",
-                 email: "teacher@example.com",
-                 password: "password",
-                 password_confirmation: "password")
+# Teacher.create!( name: "教員",
+#                  email: "teacher@example.com",
+#                  password: "password",
+#                  password_confirmation: "password")
 
-5.times do |n|
-  name = Faker::Japanese::Name.last_name
-  name2 = Faker::Japanese::Name.first_name
-  email = "user#{n+1}@example.com"
-  User.create!( name: name,
-                name2: name2,
-                email: email,
-                phone: "222222",
-                password: "password",
-                password_confirmation: "password")
+# 5.times do |n|
+#   name = Faker::Japanese::Name.last_name
+#   name2 = Faker::Japanese::Name.first_name
+#   email = "user#{n+1}@example.com"
+#   User.create!( name: name,
+#                 name2: name2,
+#                 email: email,
+#                 phone: "222222",
+#                 password: "password",
+#                 password_confirmation: "password")
 
-  name_2 = Faker::Japanese::Name.first_name
-  Child.create!( name_1: name,
-                 name_2: name_2,
-                 full_name: name + name_2,
-                 user_id: User.find_by(name: name).id,
-                 teacher_id: 1)
-end
+#   name_2 = Faker::Japanese::Name.first_name
+#   Child.create!( name_1: name,
+#                  name_2: name_2,
+#                  full_name: name + name_2,
+#                  user_id: User.find_by(name: name).id,
+#                  teacher_id: 1)
+# end
